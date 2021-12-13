314ef032d62bdbe967bd6fde0e708bfb85a0dab8
marceloleiton <marceloleiton1997@gmail.com>
2021-12-13 16:35:50 -0300

    Âmejora de seccion 3, ya no se detiene

--- seccion3.go
+++ seccion3.go
@@ -2,27 +2,23 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
 	ch := make(chan int, 3)
-	fmt.Println("Cajeros:", cap(ch), " utilizado:", len(ch))
+
 	go func() {
 		for i := 0; i < 3; i++ {
 			ch <- i
-			fmt.Println("Cliente: ", i, "Ingresa")
-			fmt.Println("Cajeros:", cap(ch), " utilizado:", len(ch))
+			fmt.Println("---> Ingresa Cliente: ", i)
+			fmt.Println("Cantidad:", cap(ch), " utilizado:", len(ch))
 		}
-		fmt.Println("No hay cupos")
+		fmt.Println("xxxxxxx Cajas Saturadas xxxxxxx")
+		close(ch)
 	}()
-
-	time.Sleep(1 * time.Second)
-
 	for valor := range ch {
-		fmt.Println("Termina su turno el usuario: ", valor)
-		fmt.Println("Cajeros:", cap(ch), " utilizado:", len(ch))
+		fmt.Println("---> Termina Cliente: ", valor)
+		fmt.Println("Cantidad:", cap(ch), " utilizado:", len(ch))
 	}
-	fmt.Println("saliendo")
-	fmt.Println("Cajeros:", cap(ch), " utilizado:", len(ch))
+	fmt.Println("Termina")
 }
