62caeec2402039dd1a7341ac0de2fe67d322fef2
= <=>
2021-12-16 01:49:36 -0300

    se termina con estructuras la seccion2.1

--- seccion21.go
+++ seccion21.go
@@ -5,30 +5,50 @@ import (
 	"time"
 )
 
-func corrutinaB(chA chan string, chB chan string) {
-	fmt.Println(<-chA) //Call B (corroutine) ↗
+type Producto struct {
+	chA chan string
+}
+
+/*
+func (p Producto) set_chA() {
+	p.chA = make(chan string)
+}
+*/
+func (p *Producto) rutina(c *Consumidor) {
+	fmt.Println("Begin A ↓")
+	p.chA <- "Call B (corroutine) ↗"
+
+	fmt.Println(<-c.chB) //Resume A
+	fmt.Println("continue in A")
+	p.chA <- "resume B"
+	fmt.Println(<-c.chB) //end B
+	fmt.Println("End A")
+}
+
+type Consumidor struct {
+	chB chan string
+}
+
+func (c *Consumidor) rutina(p *Producto) {
+	fmt.Println(<-p.chA) //Call B (corroutine) ↗
 	fmt.Println("Begin B ↓")
 	fmt.Println("continue in B")
-	chB <- "Resume A ←"
-	fmt.Println(<-chA) // Resume B
+	c.chB <- "Resume A ←"
+	fmt.Println(<-p.chA) // Resume B
 	fmt.Println("continue in B")
-	chB <- "return end B"
+	c.chB <- "return end B"
 }
 
 func main() {
 
-	chA := make(chan string)
-	chB := make(chan string)
+	consumidor := new(Consumidor)
+	productor := new(Producto)
+	productor.chA = make(chan string)
+	consumidor.chB = make(chan string)
+	//productor.set_chA()
 
-	go corrutinaB(chA, chB)
+	go productor.rutina(consumidor)
+	go consumidor.rutina(productor)
 
-	fmt.Println("Begin A ↓")
-	chA <- "Call B (corroutine) ↗"
-
-	fmt.Println(<-chB) //Resume A
-	fmt.Println("continue in A")
-	chA <- "resume B"
-	fmt.Println(<-chB) //end B
-	fmt.Println("End A")
 	time.Sleep(5 * time.Second)
 }
