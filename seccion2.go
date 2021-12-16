bb114d5b80ff6cd02cfde39ec72d7c56ad1144ca
= <=>
2021-12-16 01:52:31 -0300

    se modifica el nombre

--- seccion2.go
+++ seccion2.go
@@ -5,25 +5,50 @@ import (
 	"time"
 )
 
+type Producto struct {
+	chA chan string
+}
+
+/*
+func (p Producto) set_chA() {
+	p.chA = make(chan string)
+}
+*/
+func (p *Producto) rutina(c *Consumidor) {
+	fmt.Println("Begin A ↓")
+	p.chA <- "Call B (corroutine) ↗"
+
+	fmt.Println(<-c.chB) //Resume A
+	fmt.Println("continue in A")
+	p.chA <- "resume B"
+	fmt.Println(<-c.chB) //end B
+	fmt.Println("End A")
+}
+
+type Consumidor struct {
+	chB chan string
+}
+
+func (c *Consumidor) rutina(p *Producto) {
+	fmt.Println(<-p.chA) //Call B (corroutine) ↗
+	fmt.Println("Begin B ↓")
+	fmt.Println("continue in B")
+	c.chB <- "Resume A ←"
+	fmt.Println(<-p.chA) // Resume B
+	fmt.Println("continue in B")
+	c.chB <- "return end B"
+}
+
 func main() {
 
-	ch := make(chan string)
-	ch2 := make(chan string)
+	consumidor := new(Consumidor)
+	productor := new(Producto)
+	productor.chA = make(chan string)
+	consumidor.chB = make(chan string)
+	//productor.set_chA()
 
-	go func() {
-		fmt.Println(<-ch2)
-		time.Sleep(5 * time.Second)
-		ch <- "Capuccino :)"
-		fmt.Println(<-ch2)
-		ch <- "Terminado ."
-	}()
+	go productor.rutina(consumidor)
+	go consumidor.rutina(productor)
 
-	fmt.Println("Vendedor: Que café desea pedir?")
-	ch2 <- "Toma un tiempo para pensar"
-	valor := <-ch //Bloqueante
-
-	fmt.Println(valor)
 	time.Sleep(5 * time.Second)
-	ch2 <- "Mejor un Latte !"
-	fmt.Println("Vendedor: OK, se cambia entonces :)", <-ch)
 }
